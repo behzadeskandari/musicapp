73213ff4d0b8b8cf6a368df6361ce3466f78cadc
105624033 <behzad.b.i.g@gmail.com>
2025-11-16 11:31:29 +0330

    cmt

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -39,7 +39,8 @@
           </ul>
 
           <!-- Login Form -->
-          <vee-form v-show="tab === 'login'" :validation-schema="loginSchema">
+          <!-- :validation-schema="loginSchema" -->
+          <vee-form v-show="tab === 'login'">
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
@@ -53,8 +54,7 @@
               <label class="inline-block mb-2">Password</label>
               <vee-field :type="showPassword ? 'text' : 'password'" name="password" rules="required|min:6" as="input"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Password" />
+                duration-500 focus:outline-none focus:border-black rounded" placeholder="Enter Password" />
               <i @click="toggleShow" :class="['fas', showPassword ? 'fa-eye-slash' : 'fa-eye']"
                 class="absolute top-9 right-3 cursor-pointer text-gray-600"></i>
               <ErrorMessage name="password" class="text-red-500 text-sm mt-1" />
@@ -69,54 +69,45 @@
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
-              <vee-field type="text" name="name" as="input"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Name" />
-                 <ErrorMessage name="name" class="text-red-500 text-sm mt-1" />
+              <vee-field type="text" name="name" as="input" class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
+                duration-500 focus:outline-none focus:border-black rounded" placeholder="Enter Name" />
+              <ErrorMessage name="name" class="text-red-500 text-sm mt-1" />
             </div>
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
-              <vee-field name="email" as="input" type="email"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Email" />
-                <ErrorMessage name="email" class="text-red-500 text-sm mt-1" />
+              <vee-field name="email" as="input" type="email" class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
+                duration-500 focus:outline-none focus:border-black rounded" placeholder="Enter Email" />
+              <ErrorMessage name="email" class="text-red-500 text-sm mt-1" />
             </div>
             <!-- Age -->
             <div class="mb-3">
               <label class="inline-block mb-2">Age</label>
-              <vee-field type="number" as="input" name="age"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300
+              <vee-field type="number" as="input" name="age" class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300
                 transition duration-500 focus:outline-none focus:border-black rounded" />
-                     <ErrorMessage name="age" class="text-red-500 text-sm mt-1" />
+              <ErrorMessage name="age" class="text-red-500 text-sm mt-1" />
             </div>
             <!-- Password -->
             <div class="mb-3 relative">
               <label class="inline-block mb-2">Password</label>
-              <vee-field :type="showPassword ? 'text' : 'password'"  name="password" rules="required|min:6" as="input"
+              <vee-field :type="showPassword ? 'text' : 'password'" name="password" rules="required|min:6" as="input"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300
-                transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password" />
+                transition duration-500 focus:outline-none focus:border-black rounded" placeholder="Password" />
               <i @click="toggleShow" :class="['fas', showPassword ? 'fa-eye-slash' : 'fa-eye']"
                 class="absolute top-9 right-3 cursor-pointer text-gray-600"></i>
-                      <ErrorMessage name="password" class="text-red-500 text-sm mt-1" />
+              <ErrorMessage name="password" class="text-red-500 text-sm mt-1" />
             </div>
             <!-- Confirm Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Confirm Password</label>
-              <vee-field type="password"  name="confirm_password" rules="required|min:6" as="input"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Confirm Password" />
-                        <ErrorMessage name="confirm_password" class="text-red-500 text-sm mt-1" />
+              <vee-field type="password" name="confirm_password" rules="required|min:6" as="input" class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
+                duration-500 focus:outline-none focus:border-black rounded" placeholder="Confirm Password" />
+              <ErrorMessage name="confirm_password" class="text-red-500 text-sm mt-1" />
             </div>
             <!-- Country -->
             <div class="mb-3">
               <label class="inline-block mb-2">Country</label>
-              <select
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
+              <select class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                 duration-500 focus:outline-none focus:border-black rounded">
                 <option value="USA">USA</option>
                 <option value="Mexico">Mexico</option>
@@ -126,9 +117,12 @@
             </div>
             <!-- TOS -->
             <div class="mb-3 pl-6">
-              <input type="checkbox" class="w-4 h-4 float-left -ml-6 mt-1 rounded" />
+              <!-- <vee-field type="checkbox"  name="tos" value="1"
+              class="w-4 h-4 float-left -ml-6 mt-1 rounded" /> -->
+              <vee-field type="checkbox" name="tos" value="1" class="w-4 h-4 float-left -ml-6 mt-1 rounded" />
               <label class="inline-block">Accept terms of service</label>
             </div>
+            <ErrorMessage name="tos" class="text-red-500 text-sm mt-1" />
             <button type="submit"
               class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700">
               Submit
@@ -148,6 +142,7 @@ import { getCurrentInstance } from 'vue';
 export default {
   name: 'Auth',
   computed: {
+    ...validation,
     ...mapState(modalStore, ['hiddenClass']),
     ...mapWritableState(modalStore, {
       modalVisibility: 'isOpen'
@@ -162,13 +157,13 @@ export default {
     },
     registerSchema() {
       return {
-            name: 'required|min:2|alpha_spaces',
-            email: 'required|email',
-            age: 'required|min_value:18|max_value:120',
-            password: 'required|password|min:8',
-            confirm_password: 'required|confirmed:@password',
-            country: 'required',
-            tos: 'required',
+        name: 'required|min:2|alpha_spaces',
+        email: 'required|email',
+        age: 'required|min_value:18|max_value:120',
+        password: 'required|password|min:8',
+        confirm_password: 'required|confirmed:@password',
+        country: 'required',
+        tos: 'tos_required',
       };
     },
   }
